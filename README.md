c673a7eb6557db844f5283afa69e86d012c73ac6
inateno <inateno@gmail.com>
2019-01-24 00:36:50 +0100

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # game_sample
 A good way to get ready with the engine
+
+After clone simply do npm i && npm start
---
+++ src/data/dictionary.js
@@ -0,0 +1,23 @@
+/**
+ * @ContributorsList
+ * @Inateno / http://inateno.com / http://dreamirl.com
+ *
+ * this is the dictionary file sample that will be loaded by the project in the Localization module
+ * Please declare in the same way than this example.
+ */
+const dictionary = {
+  "en": {
+    "play": "Play",
+    "settings": "Settings",
+    "credits": "Credits",
+    "pause": "Pause"
+  },
+  "fr" : {
+    "play": "Jouer",
+    "settings": "Options",
+    "credits": "Credits",
+    "pause": "Pause"
+  }
+};
+
+export default dictionary;
